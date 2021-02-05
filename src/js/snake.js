045af41dae77804e3d1a4b91422b54c6ba54c645
Egor Riskovets <eriskovets@gmail.com>
2021-02-05 16:26:29 +0500

    мелкие исправления по code reviev от 4.02.21

--- src/js/snake.js
+++ src/js/snake.js
@@ -1,9 +1,9 @@
-const FIELD_SIZE_X = 20
-const FIELD_SIZE_Y = 20
-const SNAKESPEED = 200
+const ROWS_COUNT = 20
+const CELLS_COUNT = 20
+const SNAKE_SPEED = 200
 const snake = []
 const foodTimer = 5000
-const BOMBTIME = 10000
+const BOMB_TIME = 10000
 const LEFT_ARROW = 37
 const RIGHT_ARROW = 39
 const UP_ARROW = 38
@@ -27,15 +27,15 @@ function init() {
 function createMap() {
   const gameTable = document.createElement('table')
   gameTable.classList.add('game-table')
-  createRowsCells(FIELD_SIZE_X, FIELD_SIZE_Y, gameTable)
+  createRowsCells(gameTable)
   document.getElementById('snake-field').appendChild(gameTable) // add table
 }
 
-function createRowsCells(rows, cells, table) {
-  for (let i = 0; i < rows; i++) {
+function createRowsCells(table) {
+  for (let i = 0; i < ROWS_COUNT; i++) {
     const row = document.createElement('tr')
     row.className = `game-table-row row-${i}`;
-    for (let j = 0; j < cells; j++) {
+    for (let j = 0; j < CELLS_COUNT; j++) {
       const cell = document.createElement('td')
       cell.className = `game-table-cell cell-${i}-${j}`;
       row.appendChild(cell)
@@ -47,14 +47,14 @@ function createRowsCells(rows, cells, table) {
 function starGame() {
   gameIsRunning = true;
   respawn() // creating a snake
-  snakeTimer = setInterval(move, SNAKESPEED) // every 200 ms run function move
+  snakeTimer = setInterval(move, SNAKE_SPEED)
   setTimeout(createFood, foodTimer)
-  setInterval(createBomb, BOMBTIME)
+  setInterval(createBomb, BOMB_TIME)
 }
 
 function respawn() {
-  const startCoordX = parseInt(FIELD_SIZE_X / 2)
-  const startCoordY = parseInt(FIELD_SIZE_Y / 2)
+  const startCoordX = parseInt(ROWS_COUNT / 2)
+  const startCoordY = parseInt(CELLS_COUNT / 2)
 
   // snake's head
   const snakeHead = document.querySelector(`.cell-${startCoordY}-${startCoordX}`)
@@ -69,9 +69,9 @@ function respawn() {
 // movment of the snake
 
 function move() {
-  const coordY = parseInt(CreateSnakeCoords(snake)[1])
-  const coordX = parseInt(CreateSnakeCoords(snake)[2])
-  const newUnit = definingNewPoint(coordX, coordY, direction);
+  const coordY = parseInt(CcreateSnakeCoords(snake)[1])
+  const coordX = parseInt(CcreateSnakeCoords(snake)[2])
+  const newUnit = defineNewPoint(coordX, coordY, direction);
   if (!isSnakeUnit(newUnit) && newUnit !== undefined && !isBomb(newUnit)) {
     newUnit.setAttribute('class', `${newUnit.getAttribute('class')} snake-unit`)
     snake.push(newUnit)
@@ -81,7 +81,7 @@ function move() {
       removeTail()
     }
   } else {
-    finishTheGame()
+    finishGame()
   }
 }
 
@@ -91,13 +91,13 @@ function removeTail() {
   removed.setAttribute('class', `${classes[0]} ${classes[1]}`)
 }
 
-function CreateSnakeCoords(snake) {
+function CcreateSnakeCoords(snake) {
   const snakeHeadClasses = snake[snake.length - 1].getAttribute('class').split(' ')
   const snakeCoords = snakeHeadClasses[1].split('-')
   return snakeCoords
 }
 
-function definingNewPoint(x, y, direction) {
+function defineNewPoint(x, y, direction) {
   if (direction == 'x-') {
     newClass = buildClass(x - 1, y)
     newUnit = document.querySelector(newClass)
@@ -123,7 +123,7 @@ function isBomb(unit) {
   const unitClasses = unit.getAttribute('class').split(' ')
 
   if (unitClasses.includes('bomb-unit')) {
-    check = true;
+    check = true
   }
   return check;
 }
@@ -146,8 +146,8 @@ function haveFood(unit) {
 function createFood() {
   let foodCreated = false;
   while (!foodCreated) {
-    const foodX = parseInt(Math.random() * FIELD_SIZE_X)
-    const foodY = parseInt(Math.random() * FIELD_SIZE_Y)
+    const foodX = parseInt(Math.random() * ROWS_COUNT)
+    const foodY = parseInt(Math.random() * CELLS_COUNT)
     const foodCell = document.getElementsByClassName(`cell-${foodY}-${foodX}`)[0];
     const foodCellClasses = foodCell.getAttribute('class').split(' ')
     // cheking for a snake
@@ -169,8 +169,8 @@ function createNewUnit(cell, cellClasses, unitName) {
 function createBomb() {
   let bombCreated = false;
   while (!bombCreated) {
-    const bombX = parseInt(Math.random() * FIELD_SIZE_X)
-    const bombY = parseInt(Math.random() * FIELD_SIZE_Y)
+    const bombX = parseInt(Math.random() * ROWS_COUNT)
+    const bombY = parseInt(Math.random() * CELLS_COUNT)
     const bombCell = document.querySelector(`.cell-${bombY}-${bombX}`)
     const bombCellClasses = bombCell.getAttribute('class').split(' ')
     bombCreated = !isSnakeUnit(bombCell)
@@ -205,7 +205,7 @@ function changeDirection(event) {
   }
 }
 
-function finishTheGame() {
+function finishGame() {
   gameIsRunning = false;
   clearInterval(snakeTimer)
   alert(`Вы проиграли! Ваш результат: ${score.toString()}`)
